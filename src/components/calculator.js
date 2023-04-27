473706b80ebb6d8213ca935163dc3c2d61d96703
Eldon Victor Ratzlaff Koop <mecanicark@gmail.com>
2023-04-27 18:34:10 -0400

    Add components

---
+++ src/components/calculator.js
@@ -0,0 +1,26 @@
+const calculator = () => (
+  <section className="calculator-conteiner">
+    <div className="calculation-result">0</div>
+    <div className="operationAC">AC</div>
+    <div className="operation-change">+/-</div>
+    <div className="operation-percent">%</div>
+    <div className="operation-divition">/</div>
+    <div className="operation-multiplication">x</div>
+    <div className="operation-minus">-</div>
+    <div className="operation-add">+</div>
+    <div className="operation-iqual">=</div>
+    <div className="number9">9</div>
+    <div className="number8">8</div>
+    <div className="number7">7</div>
+    <div className="number6">6</div>
+    <div className="number5">5</div>
+    <div className="number4">4</div>
+    <div className="number3">3</div>
+    <div className="number2">2</div>
+    <div className="number1">1</div>
+    <div className="number0">0</div>
+    <div className="number-dot">.</div>
+  </section>
+);
+
+export default calculator;
